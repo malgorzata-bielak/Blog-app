d92cad2ec57a5eb3fd152d5e8f3ef0067120fd42
Malgorzata Bielak <mgw.bielak@gmail.com>
2019-04-25 11:18:49 +0200

    Final deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyCUE5GWc0MD8p2f62m-94PC3NkxO2sRkpk",
-  authDomain: "blog-app-9f510.firebaseapp.com",
-  databaseURL: "https://blog-app-9f510.firebaseio.com",
-  projectId: "blog-app-9f510",
-  storageBucket: "blog-app-9f510.appspot.com",
-  messagingSenderId: "871684134680"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
